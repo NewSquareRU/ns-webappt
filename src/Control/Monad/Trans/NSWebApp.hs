679f5b9fdfae562ae302eed0e0ec16ceff4cdee9
dpetrov <dpetrov@mabius.ru>
2016-08-25 15:54:27 +0300

    Change ExceptT for compatible with Servant exception system. Add MonadError instance

--- src/Control/Monad/Trans/NSWebApp.hs
+++ src/Control/Monad/Trans/NSWebApp.hs
@@ -15,14 +15,14 @@ module Control.Monad.Trans.NSWebApp
 import           Control.Applicative         (Alternative)
 import           Control.Monad.Base          (MonadBase (..))
 import           Control.Monad.Catch         (MonadCatch, MonadThrow)
-import           Control.Monad.Error.Class   (MonadError)
+import           Control.Monad.Error.Class   (MonadError(..))
 import           Control.Monad.IO.Class      (MonadIO (..))
 import           Control.Monad.Log           (Handler, LoggingT (..),
                                               MonadLog (..), runLoggingT)
 import           Control.Monad.Reader        (MonadReader (..), ReaderT (..))
 import           Control.Monad.Trans         (MonadTrans (..))
 import           Control.Monad.Trans.Control (MonadBaseControl (..))
-import           Control.Monad.Trans.Except  (ExceptT (..), runExceptT)
+import           Control.Monad.Trans.Except  (ExceptT (..), runExceptT, throwE, catchE)
 
 
 -- | A monad transformer that adds exceptions, read only state and logging to
@@ -40,7 +40,7 @@ import           Control.Monad.Trans.Except  (ExceptT (..), runExceptT)
 --
 newtype NSWebAppT e r message m a = NSWebAppT
     { unNSWebAppT :: ExceptT e (ReaderT r (LoggingT message m)) a
-    } deriving (Functor,Applicative,Monad,Alternative,MonadIO,MonadError e,MonadReader r,MonadThrow,MonadCatch)
+    } deriving (Functor,Applicative,Monad,Alternative,MonadIO,MonadReader r,MonadThrow,MonadCatch)
 
 instance Monad m => MonadLog message (NSWebAppT e r message m) where
     logMessage m =
@@ -55,6 +55,10 @@ instance Monad m => MonadLog message (NSWebAppT e r message m) where
 
 instance MonadTrans (NSWebAppT e r message) where
   lift = NSWebAppT . lift . lift . lift
+
+instance Monad m => MonadError e (NSWebAppT e r message m) where
+    throwError = NSWebAppT . throwE
+    catchError m h = NSWebAppT (catchE (unNSWebAppT m) (unNSWebAppT . h))
 
 deriving instance MonadBase b m => MonadBase b (NSWebAppT e r message m)
 
